52c5b63b57bd38be7b69708087cbe67ea2aa98bb
张睿 <15487952+ruizhang1@user.noreply.gitee.com>
2025-06-09 15:57:06 +0800

    feat: add gitignore

---
+++ day1/5modules.py
@@ -0,0 +1,8 @@
+from day1 import mymodule
+
+print(mymodule.say_hello())
+
+# 导入第三方模块
+import requests
+response = requests.get("https://api.github.com")
+print(response.status_code)  # 200
--- day1/read_pic.py
+++ day1/read_pic.py
@@ -1,1 +1,3 @@
 import torch
+
+print(torch.cuda.is_available())
